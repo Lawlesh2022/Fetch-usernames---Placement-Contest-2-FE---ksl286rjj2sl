90f2f921138d20da80be587b1fa398664d023680
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-10-09 23:16:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,30 @@ import React from 'react'
 import '../styles/App.css';
 import { useState, useEffect } from 'react';
 const App = () => {
- const[name, setName] = useState("");
+    const[name, setName] = useState("");
     const[data, setData] = useState("");
-
     const url = "https://content.newtonschool.co/v1/pr/main/users"
     useEffect(()=>{
        fetchData()
     }, []);
-        const fetchData = ()=>{
-            fetch(url).then((res)=>res.json()).then((response)=>{
+    const fetchData = ()=>{
+         fetch(url).then((res)=>res.json()).then((response)=>{
             console.log(response);
             setData(response)
         })
     }
+    const changeInput=(e)=>{
+      const input = e.target.value;
+      data.map((e)=>{
+        if(input==e.id){
+          setName(e.name)          
+        }
+        if(input == ""){
+          setName("")
+        }
+     })     
+
+  }
 
   return (
     <div className="App">
